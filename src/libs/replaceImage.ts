6c3f7f9e5c7bb9fe686dcb9c63449bf1a88eff38
frain <sin45kn704.coni@gmail.com>
2023-05-04 22:35:36 +0900

    Fix: thenを使う

--- src/libs/replaceImage.ts
+++ src/libs/replaceImage.ts
@@ -1,11 +1,13 @@
 import { fetchCat } from "./fetchCatApi"
 
 const loading = document.getElementById("loading");
-const image = await fetchCat();
-const catImg = document.createElement("img");
 
-catImg.src = image.url;
-catImg.width = image.width;
-catImg.height = image.height;
+fetchCat().then(image => {
+  const catImg = document.createElement("img");
 
-loading?.replaceWith(catImg);
+  catImg.src = image.url;
+  catImg.width = image.width;
+  catImg.height = image.height;
+
+  loading?.replaceWith(catImg);
+})
